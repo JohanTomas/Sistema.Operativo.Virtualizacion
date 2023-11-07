6fa02bf972603ce40030520a4634793d5f63cf15
MalasquezValerio <johan.malasquez@vallegrande.edu.pe>
2023-11-07 21:46:00 +0000

    Modificación

--- connect.js
+++ connect.js
@@ -20,3 +20,27 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
+
+// mostrar
+con.query('SELECT  * FROM actor LIMIT 100', function(error, filas){
+    if(error){
+         throw error;
+    }else{
+        filas.forEach(fila => {
+           console.log(fila);
+});
+    }
+});
+
+// insertar
+/*con.query('INSERT INTO actor (first_name, last_name, last_update) VALUES ("omar", "Rivera", "2023-11-07 12:00:00")', function(error, results){
+if(error) throw error;
+console.log('Registro Agregado', results)
+});*/
+
+
+// conexion.end();
+// ACTUALIZAR
+/*con.query('UPDATE actor SET first_name = "Steve", last_name = "Rogers" WHERE actor_id=6', function(error, results) {
+if (error) throw error;
+
